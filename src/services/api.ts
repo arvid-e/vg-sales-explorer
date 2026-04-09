c574bed4f8331107bfcf5afe5dbef5d7ef20ade6
arvid-e <arvid.edvinsson@gmail.com>
2026-04-09 15:42:18 +0900

    Make function for elastic search

--- src/services/api.ts
+++ src/services/api.ts
@@ -59,3 +59,29 @@ export const fetchGroupedGameSales = async (
     return [];
   }
 };
+
+/**
+ * Search for individual games using Elastic Search.
+ */
+export const elasticSearch = async (search: string) => {
+  const url = new URL("/api/v1/games", window.location.origin);
+  url.searchParams.append("search", search);
+
+  try {
+    const response = await fetch(url.toString());
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const games = await response.json();
+
+    if (Array.isArray(games.data)) {
+      return games.data;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.error("Could not fetch game sales:", error);
+    return [];
+  }
+};
